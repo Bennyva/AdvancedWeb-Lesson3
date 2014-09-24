443e1b0c24139d16104b816241d873187ccf960d
bennyva <benjaminvanarragon@gmail.com>
2014-09-24 07:20:39 -0400

    Added different Logo, Changed CSS, made custom classes, added Foundation 5 nav-bar (sticky)

--- AdvancedWeb-Lesson03/js/main.js
+++ AdvancedWeb-Lesson03/js/main.js
@@ -1,1 +1,21 @@
-﻿
+﻿/*$(function () {
+    $('#site-header').data('size', 'big');
+});
+
+$(window).scroll(function () {
+    var $nav = $('#site-header');
+    if ($('body').scrollTop() > 0) {
+        if ($nav.data('size') == 'big') {
+            $("div:hidden:first").fadeIn("slow");
+            $nav.data('size', 'small').stop().animate({
+                height: '300px'
+            }, 600);
+        }
+    } else {
+        if ($nav.data('size') == 'small') {
+            $nav.data('size', 'big').stop().animate({
+                height: '500px'
+            }, 600);
+        }
+    }
+});*/
