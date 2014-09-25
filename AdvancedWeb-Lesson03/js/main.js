dc84fdfa7ad3a10df02cf27bbb3ef9d3a2daa07e
Benjamin <benjaminvanarragon@gmail.com>
2014-09-24 23:14:50 -0400

    Added Image Slider, Edited CSS, JS for scroll effect,

--- AdvancedWeb-Lesson03/js/main.js
+++ AdvancedWeb-Lesson03/js/main.js
@@ -1,21 +1,4 @@
-﻿/*$(function () {
-    $('#site-header').data('size', 'big');
-});
+﻿/// <reference path="vendor/jquery.js" />
 
-$(window).scroll(function () {
-    var $nav = $('#site-header');
-    if ($('body').scrollTop() > 0) {
-        if ($nav.data('size') == 'big') {
-            $("div:hidden:first").fadeIn("slow");
-            $nav.data('size', 'small').stop().animate({
-                height: '300px'
-            }, 600);
-        }
-    } else {
-        if ($nav.data('size') == 'small') {
-            $nav.data('size', 'big').stop().animate({
-                height: '500px'
-            }, 600);
-        }
-    }
-});*/
+
+$(".wrapper").offset({top: window.innerHeight});
