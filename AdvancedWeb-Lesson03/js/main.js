89a7206c17c3e9788aad66d64c7d6a3ad81724a5
bennyva <benjaminvanarragon@gmail.com>
2014-09-25 01:29:20 -0400

    Changes CSS, added pictures, added connect panel, added icons in nav, added content, added documentation

--- AdvancedWeb-Lesson03/js/main.js
+++ AdvancedWeb-Lesson03/js/main.js
@@ -1,4 +1,11 @@
-﻿/// <reference path="vendor/jquery.js" />
+﻿/*
+    File Name : index.html
+    Author Name : Benjamin Vanarragon
+    Web Site : webdesign4.georgianc.on.ca/~200260440/AW/Portfolio
+    Description : This contains all the javascript and jquery used for my site, including smooth scrolling, highlighting the nav-bar current section etc...
+*/
+
+/// <reference path="vendor/jquery.js" />
 
 
 $(".wrapper").offset({top: window.innerHeight});
